e169ef32160b19002fbda528bfb26a3ae01634b4
dvdg6566 <czhdaniel@gmail.com>
2021-12-05 03:19:31 +0800

    oops

--- airline/airline.h
+++
@@ -1,6 +0,0 @@
-void Alice( int N, int M, int A[], int B[] );
-void InitG( int V, int U );
-void MakeG( int pos, int C, int D );
-void Bob( int V, int U, int C[], int D[] );
-void InitMap( int N, int M );
-void MakeMap( int A, int B );
